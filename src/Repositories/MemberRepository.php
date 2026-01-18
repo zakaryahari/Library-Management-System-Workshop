b2380b73169fdb6b07e7fc34e9039aa0a14398dc
zakarya hari <zakariahari42@gmail.com>
2026-01-18 17:59:46 +0100

    feat: implémentation de findById avec gestion du polymorphisme (Student/Faculty) .

--- src/Repositories/MemberRepository.php
+++ src/Repositories/MemberRepository.php
@@ -5,6 +5,10 @@ namespace src\Repositories;
 use src\Repositories\DatabaseConnection;
 use src\Models\Book;
 
+use src\Models\Member;
+use src\Models\StudentMember;
+use src\Models\FacultyMember;
+
 class MemberRepository {
     private DatabaseConnection $db;
 
@@ -13,7 +17,23 @@ class MemberRepository {
     }
 
     public function findById(int $id): ?Member {
-        return null;
+        $sql = "SELECT * FROM members WHERE id = :id";
+        $query = $this->db->getConnection()->prepare($sql);
+        $query->bindValue(":id", $id, PDO::PARAM_INT);
+        $query->execute();
+        $result = $query->fetch();
+
+        if (!$result) {
+            return null;
+        }
+
+        if ($result['member_type'] == 'Student') {
+            return new StudentMember($this->db , $result['id'], $result['name'], $result['email'], $result['membership_date'], $result['member_type'], $result['school_name']);
+        }
+
+        if ($result['member_type'] == 'Faculty') {
+            return new FacultyMember($this->db , $result['id'], $result['name'], $result['email'], $result['membership_date'], $result['member_type'], $result['school_name']);
+        }
     }
 
     public function updateFees(int $id, float $amount): bool {
